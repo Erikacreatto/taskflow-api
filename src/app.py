679b878450609fd2fb9bb2102ee660396f0a5cb1
Erikacreatto <erikacreatto@gmail.com>
2026-06-12 14:40:52 -0300

    Atualiza descrição da página sobre

--- src/app.py
+++ src/app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return "Sistema de Gerenciamento de Biblioteca"
 
+@app.route("/sobre")
+def sobre():
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
