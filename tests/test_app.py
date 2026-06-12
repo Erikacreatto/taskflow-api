a412bffe08f6486c08c10f24fcf910ee31b03bfc
Erikacreatto <erikacreatto@gmail.com>
2026-06-12 16:41:05 -0300

    test: adiciona teste da rota home

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("."))
+
+from src.app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
