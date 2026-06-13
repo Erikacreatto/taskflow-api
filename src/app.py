aa68d1d4b2f5acaf1769f4b5bb49c4a0381fa88a
Erikacreatto <erikacreatto@gmail.com>
2026-06-13 11:44:21 -0300

    feat: altera mensagem final

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sistema de Gestão de Biblioteca Online"
+    return "Sistema de Gestão de Biblioteca Online - Versão Atualizada por Erika"
 
 @app.route("/sobre")
 def sobre():
